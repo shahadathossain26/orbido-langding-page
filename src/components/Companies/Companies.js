62232e728d512137d5f43bf77fec9057d0da304b
shahadathossain26 <shiimran172372@gmail.com>
2023-04-14 17:02:54 +0600

    campanies section done

--- src/components/Companies/Companies.js
+++ src/components/Companies/Companies.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import airbnb from '../../assets/company_logos/airbnb.png'
+import binance from '../../assets/company_logos/binance.png'
+import coinbase from '../../assets/company_logos/coinbase.png'
+import dropbox from '../../assets/company_logos/dropbox.png'
 
 const Companies = () => {
     return (
-        <div>
-            This is companies.
+        <div className='lg:mt-[183px] mx-[187px] flex justify-between items-center'>
+            <img src={airbnb} alt="airbnb" />
+            <img src={binance} alt="binance" />
+            <img src={coinbase} alt="coinbase" />
+            <img src={dropbox} alt="dropbox" />
         </div>
     );
 };
